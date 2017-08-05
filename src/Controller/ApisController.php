0ef8680a683b80e179ff89e035de6afdf6344c95
zalsader <zalsader@hotmail.com>
2017-08-05 05:31:43 +0300

    Send SMS on status change

--- src/Controller/ApisController.php
+++ src/Controller/ApisController.php
@@ -49,6 +49,7 @@ class ApisController extends AppController
 
     public function subscribe()
     {
+        // TODO check signature
         if ($this->request->is('get')) {
             if ($this->request->getQuery('hub_mode') == 'subscribe' && $this->request->getQuery('hub_verify_token')) {
                 $this->response = $this->response->withStringBody($this->request->getQuery('hub_challenge'));
@@ -57,21 +58,51 @@ class ApisController extends AppController
         }
         $request = $this->request->input('json_decode');
         Log::debug(json_encode($request));
+        foreach ($request['entry'] as $entry) {
+            $userId = $entry['uid'];
+            $userName = $this->getUserName($userId);
+            foreach ($entry['changes'] as $change) {
+                if ($change['field'] == 'status') {
+                    $message = "Your friend $userName changed his ststus to: " . $change['value'];
+                    $this->sendSms('+1 256-567-5764', $message);
+                }
+            }
+        }
         $this->response = $this->response->withStringBody('OK');
         return $this->response;
     }
 
-    public function sendSms() {
-        if (Configure::read('Twilio.sendSmsEnabled')) {
-            $client = new Client(Configure::read('Twilio.accountSid'), Configure::read('Twilio.authToken'));
-            $sms = $client->account->messages->create(
-                '+1 256-567-5764',
-                array(
-                    'from' => Configure::read('Twilio.messagingSid'),
-                    'body' => "Hey Test, Monkey Party at 6PM. Bring Bananas!"
-                )
-            );
-            debug($sms); exit;
+    private function getUserName($userId) {
+        $fb = new Facebook([
+            'app_id' => Configure::read('Facebook.appId'),
+            'app_secret' => Configure::read('Facebook.secret'),
+            'default_graph_version' => Configure::read('version'),
+            'default_access_token' => $this->request->getData('fbtoken'),
+        ]);
+
+        try {
+            $response = $fb->get('/me?fields=id,name');
+            $userNode = $response->getGraphUser();
+            return $userNode->getName();
+        } catch (Exception $e) {
+            return 'Unknown';
         }
+    }
+
+    private function sendSms($to, $message) {
+        $client = new Client(Configure::read('Twilio.accountSid'), Configure::read('Twilio.authToken'));
+        $sms = $client->account->messages->create(
+            $to,
+            array(
+                'from' => Configure::read('Twilio.messagingSid'),
+                'body' => $message
+            )
+        );
+        return $sms;
+    }
+
+    public function sendSmsRequest() {
+            $sms = $this->sendSms('+1 256-567-5764', 'Hey Test, Monkey Party at 6PM. Bring Bananas!');
+            debug($sms); exit;
     }
 }
